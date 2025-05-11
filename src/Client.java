80044b622b73537e8f5acce67dffc24eb26b4e4c
osegala <owen.segala@gmail.com>
2025-05-11 19:55:32 -0400

    Changed the client to accept files, and separated the clientHandler and Server classes

--- src/Client.java
+++ src/Client.java
@@ -1,48 +1,152 @@
 import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.nio.file.Files;
+//import java.net.UnknownHostException;
 import java.util.Scanner;
-
-import java.awt.Image;
+//import java.awt.Image;
 
 public class Client {
-    public static final String FILES_PATH = "./src/files";
+    public static final String FILES_PATH = "files";
     private Socket clientSock;
     private DataInputStream receiveData;
     private DataOutputStream sendData;
     private Scanner write;
-    private String line;
+    private String username;
 
     public Client() {
-        try{
-            clientSock = new Socket("127.0.0.1",Server.PORT);
-            receiveData = new DataInputStream(new BufferedInputStream(clientSock.getInputStream()));
-            sendData = new DataOutputStream(clientSock.getOutputStream());
-            write = new Scanner(System.in);
-            while(true){
-                clientMessages();
+    try {
+        write = new Scanner(System.in);
+        System.out.print("Enter your Username: ");
+        username = write.nextLine();
+
+        clientSock = new Socket("127.0.0.1", Server.PORT);
+
+        receiveData = new DataInputStream(new BufferedInputStream(clientSock.getInputStream()));
+        sendData = new DataOutputStream(clientSock.getOutputStream());
+
+        sendData.writeUTF("USER|" + username);
+        sendData.flush();
+
+        new Thread(this::listenForMessages).start();
+
+        while (true) {
+            clientMessages();
+        }
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+    private void listenForMessages() {
+        try {
+             while (true) {
+                String incoming = receiveData.readUTF();
+                String[] parts = incoming.split("\\|", 4);
+
+                 if (parts.length == 4 && parts[0].equals("MSG")) {
+                    String to = parts[1];
+                    String from = parts[2];
+                    String content = parts[3];
+
+                    if (to.equalsIgnoreCase("ALL")) {
+                        System.out.println("\n[" + from + " → Everyone]: " + content);
+                    } else if (to.equalsIgnoreCase(username)) {
+                        System.out.println("\n[" + from + " → You]: " + content);
+                    } else {
+                        System.out.println("\n[" + from + " → " + to + "]: " + content);
+                    }
+
+                    } else if (parts.length == 4 && parts[0].equals("FILE")) {
+                        String to = parts[1];
+                        String from = parts[2];
+                        String filename = parts[3];
+
+                    int length = receiveData.readInt();
+                    byte[] fileBytes = new byte[length];
+                    receiveData.readFully(fileBytes);
+
+                    File folder = new File("received");
+                    if (!folder.exists()) {
+                        folder.mkdir();
+                    }
+
+                    File outFile = new File(folder, filename);
+                    Files.write(outFile.toPath(), fileBytes);
+
+                    System.out.println("\n[File Received from " + from + "] Saved: received/" + filename);
+                } else {
+                    System.out.println("\n[Received]: " + incoming);
+                }
             }
-            //close();
-        } catch(IOException e){
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Disconnected from server.");
         }
     }
+
+
+    private void sendFile(String recipient) throws IOException {
+        Menu(); // show file options
+        System.out.print("Enter the number of the file to send: ");
+        int fileIndex = Integer.parseInt(write.nextLine()) - 1;
+
+        File[] files = new File(FILES_PATH).listFiles();
+        if (fileIndex < 0 || fileIndex >= files.length) {
+            System.out.println("Invalid file number.");
+            return;
+        }
+
+        File selectedFile = files[fileIndex];
+        byte[] fileBytes = Files.readAllBytes(selectedFile.toPath());
+
+        String header = "FILE|" + recipient + "|" + username + "|" + selectedFile.getName();
+        sendData.writeUTF(header);
+        sendData.writeInt(fileBytes.length);
+        sendData.write(fileBytes);
+        sendData.flush();
+
+        System.out.println("Sent file: " + selectedFile.getName() + " to " + recipient);
+    }
+
 
     //Creates messages that the clients sends to the handler to be processed to the server
-    private void clientMessages() throws IOException{
-        //We want to use this String as the data we send to the server
-        String message = null;
+    private void clientMessages() throws IOException {
+        while (true) {
+            System.out.println("Send to (type username or 'ALL' for broadcast): ");
+            String recipient = write.nextLine().trim();
 
-        //This is an intermediary variable that will build message
-        line = "";
+            System.out.println("Choose action:");
+            System.out.println("[1] Send Message");
+            System.out.println("[2] Send File");
+            System.out.println("[3] Quit");
 
-        Menu();
-        while(!line.equals(" ")){
-            line = write.nextLine();
-            sendData.writeUTF(message);
-        }
+            String choice = write.nextLine().trim();
 
+            if (choice.equals("3") || choice.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            switch (choice) {
+                case "1":
+                    System.out.print("Enter your message: ");
+                    String message = write.nextLine();
+                    String msg = "MSG|" + recipient + "|" + username + "|" + message;
+                    sendData.writeUTF(msg);
+                    sendData.flush();
+                break;
+
+                case "2":
+                    sendFile(recipient);
+                    break;
+
+                 default:
+                     System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+            }
+        }   
     }
+
 
     //Displays all files within the given folder
     private void Menu() throws IOException{
--- src/Server.java
+++ src/Server.java
@@ -1,16 +1,15 @@
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+//import java.io.BufferedInputStream;
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+//import javax.imageio.ImageIO;
+//import javax.swing.ImageIcon;
+//import javax.swing.JLabel;
 
 public class Server {
     public static final int PORT = 3030;
@@ -34,133 +33,52 @@ public class Server {
     private void acceptClients() throws IOException {
         Socket var1 = this.server.accept();
         ++this.clientCount;
-        String var2 = "User" + this.clientCount;
-        ClientHandler type = new ClientHandler(var1, var2);
+        ClientHandler type = new ClientHandler(this, var1, null); // Username will be set later
         this.clients.add(type);
         (new Thread(type)).start();
-        System.out.println(var2 + " connected.");
+        System.out.println("New client connected. Waiting for username...");   
     }
 
-    public void broadcastMessage(String var1, String var2) {
-        for(ClientHandler recipients : this.clients) {
-            recipients.sendText("MSG|ALL|" + var1 + "|" + var2);
+    public void broadcastMessage(String sender, String message) {
+        for (ClientHandler recipient : this.clients) {
+        recipient.sendText("MSG|ALL|" + sender + "|" + message);
         }
-
     }
 
-    public void sendMessageTo(String var1, String var2, String type) {
-        for(ClientHandler sender : this.clients) {
-            if (sender.getUsername().equals(var2)) {
-                sender.sendText("MSG|" + var2 + "|" + var1 + "|" + type);
+
+    public void sendMessageTo(String sender, String recipient, String message) {
+        for (ClientHandler client : this.clients) {
+            if (client.getUsername().equalsIgnoreCase(recipient)) {
+                client.sendText("MSG|" + recipient + "|" + sender + "|" + message);
                 break;
             }
         }
-
     }
 
-    public void broadcastFile(String var1, String var2, byte[] type) {
-        for(ClientHandler sender : this.clients) {
-            sender.sendFile("FILE|ALL|" + var1 + "|" + var2, type);
+
+    public void broadcastFile(String sender, String filename, byte[] data) {
+        for (ClientHandler client : this.clients) {
+            client.sendFile("FILE|ALL|" + sender + "|" + filename, data);
         }
-
     }
 
-    public void sendFileTo(String var1, String var2, String type, byte[] recipients) {
-        for(ClientHandler content : this.clients) {
-            if (content.getUsername().equals(var2)) {
-                content.sendFile("FILE|" + var2 + "|" + var1 + "|" + type, recipients);
+
+    public void sendFileTo(String sender, String recipient, String filename, byte[] data) {
+        for (ClientHandler client : this.clients) {
+            if (client.getUsername().equalsIgnoreCase(recipient)) {
+                client.sendFile("FILE|" + recipient + "|" + sender + "|" + filename, data);
                 break;
             }
         }
+    }
 
+
+    public void removeClient(ClientHandler client){
+        clients.remove(client);
     }
 
     public static void main(String[] var0) {
         new Server();
     }
-
-    private class ClientHandler implements Runnable {
-        private Socket socket;
-        private DataInputStream in;
-        private DataOutputStream out;
-        private String username;
-
-        public ClientHandler(Socket var2, String type) {
-            this.socket = var2;
-            this.username = type;
-
-            try {
-                this.in = new DataInputStream(new BufferedInputStream(var2.getInputStream()));
-                this.out = new DataOutputStream(var2.getOutputStream());
-            } catch (IOException sender) {
-                sender.printStackTrace();
-            }
-
-        }
-
-        public String getUsername() {
-            return this.username;
-        }
-
-        public void run() {
-            try {
-                while(true) {
-                    String clientMessage = this.in.readUTF();
-                    String[] headers = clientMessage.split("\\|", 4);
-                    String type = headers[0];
-                    String recipients = headers[1];
-                    String sender = headers[2];
-                    String content = headers[3];
-                    if ("MSG".equals(type)) {
-                        if ("ALL".equals(recipients)) {
-                            Server.this.broadcastMessage(sender, content);
-                        } else {
-                            Server.this.sendMessageTo(sender, recipients, content);
-                        }
-                    } else if ("FILE".equals(type)) {
-                        int var7 = this.in.readInt();
-                        byte[] var8 = new byte[var7];
-                        this.in.readFully(var8);
-                        if ("ALL".equals(recipients)) {
-                            Server.this.broadcastFile(sender, content, var8);
-                        } else {
-                            Server.this.sendFileTo(sender, recipients, content, var8);
-                        }
-                    }
-                }
-            } catch (IOException var16) {
-                System.out.println(this.username + " disconnected.");
-            } finally {
-                try {
-                    this.socket.close();
-                } catch (IOException var15) {
-                }
-
-                Server.this.clients.remove(this);
-            }
-
-        }
-
-        void sendText(String var1) {
-            try {
-                this.out.writeUTF(var1);
-                this.out.flush();
-            } catch (IOException type) {
-                type.printStackTrace();
-            }
-
-        }
-
-        void sendFile(String var1, byte[] var2) {
-            try {
-                this.out.writeUTF(var1);
-                this.out.writeInt(var2.length);
-                this.out.write(var2);
-                this.out.flush();
-            } catch (IOException recipients) {
-                recipients.printStackTrace();
-            }
-
-        }
-    }
+   
 }
